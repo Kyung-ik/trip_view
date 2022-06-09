745eb8be8947ce17892219f2e2b008bf20e8490e
KyungIk <102667478+Kyung-ik@users.noreply.github.com>
2022-06-09 16:47:00 +0900

    spring

---
+++ tripviewBoard/src/main/java/com/tripviewBoard/springboot/TripviewBoardApplication.java
@@ -0,0 +1,13 @@
+package com.tripviewBoard.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripviewBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripviewBoardApplication.class, args);
+	}
+
+}
---
+++ tripviewBoard/src/test/java/com/tripviewBoard/springboot/TripviewBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripviewBoard.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripviewBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
